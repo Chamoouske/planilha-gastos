f7f40690a49907faac4df98037c4c5497734a35b
Ajax Lima <ajaxlima94@gmail.com>
2024-11-16 17:48:50 -0300

    initial commit

---
+++ backend/src/main/java/br/com/planilha/gastos/PlanilhaDeGastosApplication.java
@@ -0,0 +1,13 @@
+package br.com.planilha.gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanilhaDeGastosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanilhaDeGastosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/planilha/gastos/PlanilhaDeGastosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.planilha.gastos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanilhaDeGastosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
